9694314b5fb583e535bd7c9f9dafcd477d44577b
eslammMo <eslammohmed2001@gmail.com>
2022-10-07 00:38:01 +0200

    add Product-1

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -10,5 +10,6 @@ public class ApplicationDbContext : DbContext
     }
     public   DbSet<Category> categories { get; set; }
     public DbSet<CoverType> CoverTypes { get; set; }
+    public DbSet<Product> Products { get; set; }
 
 }
